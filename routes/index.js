ab25309225049e75ce22971636af8ec5f159b524
victor77dev <victor77.dev@gmail.com>
2018-05-25 15:49:43 +0200

    Get paper info from arXiv using paper title

--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,7 @@ var express = require('express');
 var router = express.Router();
 
 const dlFile = require('../lib/downloadFile');
+const arXiv = require('../lib/arXivLib');
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
@@ -18,6 +19,17 @@ router.get('/testDownloadFile', function(req, res, next) {
   }).catch((err) => {
     console.log(err);
     res.render('index', { title: 'Failed to download link' });
+  })
+});
+
+router.get('/testGetPaperInfo', function(req, res, next) {
+  // Testing get paper info from arXiv using paper title
+  const paperTitle = 'Synthetic and Natural Noise Both Break Neural Machine Translation';
+  arXiv.getInfo(paperTitle).then((paperInfo) => {
+    res.render('index', { title: JSON.stringify(paperInfo) });
+  }).catch((err) => {
+    console.log(err);
+    res.render('index', { title: 'Failed to get paper info' });
   })
 });
 
