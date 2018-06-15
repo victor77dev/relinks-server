190815801d55dee653e8ff781157bc2347475c29
victor77dev <victor77.dev@gmail.com>
2018-06-15 18:59:03 +0200

    Add CORS (Allow Cross-Origin)

--- app.js
+++ app.js
@@ -22,6 +22,14 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+// Allow Cross-Origin Resource Sharing (CORS)
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header('Access-Control-Allow-Credentials', true);
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
 app.use('/', index);
 app.use('/users', users);
 
